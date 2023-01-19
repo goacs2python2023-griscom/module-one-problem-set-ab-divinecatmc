123eaf23f02c6c1f547893404d7027a5e6b1a4c3
Devin Atalay <devinatalay@Devins-MacBook-Air.local>
2023-01-19 09:24:30 -0800

    I tried to enter

---
+++ square.py
@@ -0,0 +1,5 @@
+side = int(input("Enter Side Length: "))
+
+square = side * side
+
+print("Area of Square: " + str(square))
---
+++ twodice.py
@@ -0,0 +1,22 @@
+import random
+
+def die():
+
+    diceone = random.randint(1,6)
+
+    dicetwo = random.randint(1,6)
+
+    sum = diceone+dicetwo
+    if sum == 6 or sum == 7 or sum == 8:
+
+        print(str(sum))
+
+        return ("win")
+    
+    else:
+
+        print(str(sum))
+
+        return("lose")
+
+die()
